a84fe29b0bc6c299653a68fd793bc1dd2315f8f1
RAFAEL HAINTZ DA SILVA <rafael.silva22@alunos.sc.senac.br>
2025-05-22 11:01:48 -0300

    primeiro ommit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler"; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+ 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { StatusBar } from "expo-status-bar"; 
+
+import React from "react"; 
+
+import { AppLoading } from "expo"; 
+
+import { useFonts, Anton_400Regular } from "@expo-google-fonts/anton"; 
+
+ 
+
+import Routes from "./src/router"; 
+
+ 
+
+export default function App() { 
+
+  let [fontsLoaded] = useFonts({ 
+
+    Anton_400Regular, 
+
+  }); 
+
+ 
+
+  // if (!fontsLoaded) { 
+
+  //   return <AppLoading />; 
+
+  // } 
+
+ 
+
+  return ( 
+
+    <> 
+
+      <StatusBar style="light" backgroundColor="#000" translucent={true} /> 
+
+      <Routes /> 
+
+    </> 
+
+  ); 
+
+} 
